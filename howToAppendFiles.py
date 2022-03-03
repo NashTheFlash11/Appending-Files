8f1e53bcbc848091c50fda90f4bb3b68078397e4
NashTheFlash11 <71734964+NashTheFlash11@users.noreply.github.com>
2022-03-02 19:13:38 -0500

    Add files via upload

---
+++ howToAppendFiles.py
@@ -0,0 +1,4 @@
+with open("Python/AppendingFiles/newFile.txt", 'a') as firstFile:
+    firstFile.write("Hi there! I'm NashTheFlash.")
+    firstFile.write("\n")
+    firstFile.write("this sample text")
